72901da553fe928d85c734e5e4de6dd203494e01
leonliu06 <leonliu06@gmail.com>
2017-03-27 15:30:30 +0800

    external jars must be included in /WEB-INF/lib and '/' begins after appname

---
+++ app03b/src/app03b/controller/SaveProductController.java
@@ -0,0 +1,43 @@
+package app03b.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import app03b.domain.Product;
+import app03b.form.ProductForm;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class SaveProductController implements Controller {
+	
+	private static final Log logger = LogFactory.getLog(SaveProductController.class);
+	
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		
+		logger.info("SaveProductController caller");
+		
+		ProductForm productForm = new ProductForm();
+		// populate form properties
+		productForm.setName(request.getParameter("name"));
+		productForm.setDescription(request.getParameter("description"));
+		productForm.setPrice(request.getParameter("price"));
+		
+		// create model
+		Product product = new Product();
+		product.setName(productForm.getName());
+		product.setDescription(productForm.getDescription());
+		try{
+			product.setPrice(Float.parseFloat(productForm.getPrice()));
+		}catch(NumberFormatException e){
+			
+		}
+		
+		// insert code to add product to the database
+		
+		return new ModelAndView("ProductDetails", "product", product);
+	}
+}
